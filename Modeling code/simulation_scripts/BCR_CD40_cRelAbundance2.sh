a300613c3651908f844a04a0b278af887dcca899
Helen Huang (Xin) <helenhuang@g.ucla.edu>
2024-02-09 14:32:35 -0800

    Add files via upload

---
+++ Modeling code/simulation_scripts/BCR_CD40_cRelAbundance2.sh
@@ -0,0 +1,22 @@
+source /home/helen/.bashrc
+export JULIA_NUM_THREADS=50
+
+home_dir="/home/helen/BCELL_PROJECT/"
+
+for i in {6..10}
+do
+    # With competitive inhibition
+    modifier="cRelAbundance_KOF${i}"
+    julia $home_dir'simulation_scripts/mainHaricRel.jl' -v "spawn" -o $home_dir'results/'$modifier'.txt' -i $home_dir'data/steady_'$modifier'.jld' -c $home_dir'data/cells_'$modifier'.jld' -p $home_dir'params/Param1_cRel_IKBEKO.jl' >> $home_dir'job-logs/'$modifier'.out'
+
+    modifier="cRelAbundance_WTF${i}"
+    julia $home_dir'simulation_scripts/mainHaricRel.jl' -v "spawn" -o $home_dir'results/'$modifier'.txt' -i $home_dir'data/steady_'$modifier'.jld' -c $home_dir'data/cells_'$modifier'.jld' -p $home_dir'params/Param1_cRel_WT.jl' >> $home_dir'job-logs/'$modifier'.out'
+
+    # No competitive inhibition
+    modifier="cRelAbundance_WTF${i}n"
+    julia $home_dir'simulation_scripts/mainHaricRel.jl' -v "spawn" -o $home_dir'results/'$modifier'.txt' -i $home_dir'data/steady_'$modifier'.jld' -c $home_dir'data/cells_'$modifier'.jld' -p $home_dir'params/Param2_cRel_WT.jl' >> $home_dir'job-logs/'$modifier'.out'
+
+    modifier="cRelAbundance_KOF${i}n"
+    julia $home_dir'simulation_scripts/mainHaricRel.jl' -v "spawn" -o $home_dir'results/'$modifier'.txt' -i $home_dir'data/steady_'$modifier'.jld' -c $home_dir'data/cells_'$modifier'.jld' -p $home_dir'params/Param2_cRel_IKBEKO.jl' >> $home_dir'job-logs/'$modifier'.out'
+
+done
